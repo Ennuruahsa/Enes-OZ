c09a521b7408fd5b098b1121644dfc1b80049a67
Enes ÖZ <153341797+Ennuruahsa@users.noreply.github.com>
2024-02-04 15:19:32 +0100

    T-SQL

--- bibliothek.sql
+++ bibliothek.sql
@@ -1,17 +1,17 @@
-
-
 USE bibliothek;
+GO
+
 CREATE TABLE Buecher(
     BuchID INT PRIMARY KEY,
     Title VARCHAR(200),
     AuthorID INT,
     HerausgeberID INT,
-    Veröffenlichesdatum Date,
+    Veroeffentlichungsdatum DATE, 
     Menge INT
 );
 
 CREATE TABLE Author(
-    AuhorID INT PRIMARY KEY,
+    AuthorID INT PRIMARY KEY, 
     Vorname VARCHAR(50),
     Nachname VARCHAR(50)
 );
@@ -19,43 +19,31 @@ CREATE TABLE Author(
 CREATE TABLE Herausgeber (
     HerausgeberID INT PRIMARY KEY,
     Name_He VARCHAR(100),
-    Adresse VARCHAR(200),
+    Adresse VARCHAR(200) 
 );
 
--- Wir schreiben die Values dazu
+-- Werte hinzufügen
 
-USE bibliothek;
+INSERT INTO Author (AuthorID, Vorname, Nachname)
+VALUES (51, 'Bertus', 'Aafjes'),
+        (12, 'Patrika', 'Aakhus'),
+        (17, 'Hans', 'Aanrut');
 
-INSERT INTO Author (AuhorID, Vorname, Nachname)
-VALUES (51,'Bertus', 'Aafjes'),
-        (12,'Patrika', 'Aakhus'),
-        (17,'Hans','Aanrut');
+INSERT INTO Herausgeber(HerausgeberID, Name_He, Adresse)
+VALUES (155, 'Econ Verlag', 'Münih'),
+        (178, 'XYZ Verlag', 'Münster'),
+        (231, 'Knut Verlag', 'Hamburg');
 
-INSERT INTO Herausgeber(HerausgeberID,Name_He,Adresse)
-VALUES (155,'Econ Verlag','Münih'),
-        (178,'XYZ Verlag','Münster'),
-        (231,'Knut Verlag','Hamburg');
+INSERT INTO Buecher(BuchID, Title, AuthorID, HerausgeberID, Veroeffentlichungsdatum, Menge) 
+VALUES (13, 'Amoureus liedje in de morgenstond', 51, 155, '2000-01-05', 200),
+        (72, 'Ohne Aktien wird schwer', 12, 178, '2020-05-20', 500),
+        (92, '12 Gesetze der Dummheit', 17, 231, '2018-12-01', 430);
 
-INSERT INTO Buecher(BuchID,Title,AuthorID,HerausgeberID,Veröffenlichesdatum,Menge)
-VALUES (13,'Amoureus liedje in de morgenstond',51,155,'2000-01-05',200),
-        (72,'Ohne Aktien wird schwer',12,178,'2020-05-20',500),
-        (92,'12 Gesetze der Dummheit',17,231,'2018-12-01',430);
+-- Transaction --
 
-
-
---Transaction
-
-begin transaction -- Anfang des Codes
-UPDATE Buecher where BuchID=13 set Menge=100,
-Select * from bibliothek 
--- rolback -- Stornierung
--- Select * from bibliothek
-Commit
-
-
-
-
-
-
-
-
+BEGIN TRANSACTION; -- Anfang des Codes
+UPDATE Buecher SET Menge = 100 WHERE BuchID = 13; 
+SELECT * FROM Buecher; 
+-- ROLLBACK; -- Stornierung
+-- SELECT * FROM Buecher;
+COMMIT;
